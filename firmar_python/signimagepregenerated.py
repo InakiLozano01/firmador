c27b26ee47f5f508c881b4f5e23660a18559839f
Inaki Lozano <kakitolozano@gmail.com>
2024-06-24 12:51:00 -0300

    Cambios para enviar solo imagen a api de dss ya escalada con firma y sello

--- firmar_python/signimagepregenerated.py
+++ firmar_python/signimagepregenerated.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 import io
 import base64
 
-def create_signature_image(text, encoded_image, width=234, height=57, scale_factor=5):
+def create_signature_image(text, encoded_image, width=234, height=57, scale_factor=50):
     # Create a new image with white background at higher resolution
     high_res_width = width * scale_factor
     high_res_height = height * scale_factor
@@ -30,15 +30,15 @@ def create_signature_image(text, encoded_image, width=234, height=57, scale_fact
     stamp_data = base64.b64decode(encoded_image)
     stamp = Image.open(io.BytesIO(stamp_data))
     
-    # Resize stamp to fit within the remaining width and height
+    # Resize stamp to fit within the remaining width and height, taking into account the scale factor
     stamp_max_width = high_res_width - text_width - 5 * scale_factor  # 5 pixels padding
     stamp_max_height = high_res_height - 4 * scale_factor  # 2 pixels padding top and bottom
-    stamp.thumbnail((stamp_max_width, stamp_max_height))
+    stamp.thumbnail((stamp_max_width, stamp_max_height), Image.Resampling.LANCZOS)
     
     # Calculate position to paste stamp (right-aligned)
     stamp_x = high_res_width - stamp.width - 2 * scale_factor  # 2 pixels padding from right edge
     stamp_y = (high_res_height - stamp.height) // 2
-    
+
     # Paste stamp image
     img.paste(stamp, (stamp_x, stamp_y), stamp if stamp.mode == 'RGBA' else None)
     
