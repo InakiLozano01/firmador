a0724ad4e57b389f08d884de5cfeea7015136c9a
Inaki Lozano <kakitolozano@gmail.com>
2024-06-24 12:23:04 -0300

    Cambios para enviar solo imagen a api de dss ya escalada con firma y sello

--- firmar_python/signimagepregenerated.py
+++ firmar_python/signimagepregenerated.py
@@ -27,22 +27,22 @@ def create_signature_image(text, encoded_image, width=234, height=57, dpi=300):
     
     # Split text into lines and draw each line
     lines = text.split('\n')
-    y_text = 5 * dpi // 72
+    y_text = int(5 * dpi / 72)
     for line in lines:
-        draw.text((5 * dpi // 72, y_text), line, font=font, fill='black')
-        y_text += font.getbbox(line)[3] + 2 * dpi // 72  # Move to next line (font height + 2 pixels)
+        draw.text((int(5 * dpi / 72), y_text), line, font=font, fill='black')
+        y_text += font.getbbox(line)[3] + int(2 * dpi / 72)  # Move to next line (font height + 2 pixels)
     
     # Decode and open the stamp image
     stamp_data = base64.b64decode(encoded_image)
     stamp = Image.open(io.BytesIO(stamp_data))
     
     # Resize stamp to fit within the remaining width and height
-    stamp_max_width = new_width - text_width - 5 * dpi // 72  # 5 pixels padding
-    stamp_max_height = new_height - 4 * dpi // 72  # 2 pixels padding top and bottom
+    stamp_max_width = new_width - text_width - int(5 * dpi / 72)  # 5 pixels padding
+    stamp_max_height = new_height - int(4 * dpi / 72)  # 2 pixels padding top and bottom
     stamp.thumbnail((stamp_max_width, stamp_max_height))
     
     # Calculate position to paste stamp (right-aligned)
-    stamp_x = new_width - stamp.width - 2 * dpi // 72  # 2 pixels padding from right edge
+    stamp_x = new_width - stamp.width - int(2 * dpi / 72)  # 2 pixels padding from right edge
     stamp_y = (new_height - stamp.height) // 2
     
     # Paste stamp image
