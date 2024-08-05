11a50be53b156ba86805e01f89d02f979c6df069
Inaki Lozano <kakitolozano@gmail.com>
2024-08-05 10:58:44 -0300

    importaciones resueltas y cambio de puertos

--- firmar_python/dss_sign.py
+++ firmar_python/dss_sign.py
@@ -161,7 +161,7 @@ def sign_document_own(pdf, signature_value, certificates, current_time, field_id
                         "width": None,
                         "height": None,
                         "rotation": None,
-                        "page": len(PdfReader(io.BytesIO(base64.b64decode(pdf))).pages)
+                        "page": len(PdfFileReader(io.BytesIO(base64.b64decode(pdf))).pages)
                     },
                     "textParameters": None,
                     "zoom": None
@@ -446,7 +446,7 @@ def get_data_to_sign_tapir(pdf, certificates, current_time, field_id, stamp, enc
                         "width": None,
                         "height": None,
                         "rotation": None,
-                        "page": len(PdfReader(io.BytesIO(base64.b64decode(pdf))).pages)
+                        "page": len(PdfFileReader(io.BytesIO(base64.b64decode(pdf))).pages)
                     },
                     "textParameters": None,
                     "zoom": None
@@ -543,7 +543,7 @@ def sign_document_tapir(pdf, signature_value, certificates, current_time, field_
                         "width": None,
                         "height": None,
                         "rotation": None,
-                        "page": len(PdfReader(io.BytesIO(base64.b64decode(pdf))).pages)
+                        "page": len(PdfFileReader(io.BytesIO(base64.b64decode(pdf))).pages)
                     },
                     "textParameters": None,
                     "zoom": None
